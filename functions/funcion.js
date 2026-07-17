78112c518f32ec7df1af83f3182b6f6e5983fb7b
Nordcode-cyber@users.noreply.github.com <88049567+Nordevelop@users.noreply.github.com>
2026-07-16 22:49:08 -0300

    changed folder

--- functions/funcion.js
+++ functions/funcion.js
@@ -9,4 +9,12 @@ function mostrarmensaje1(){
     console.log(mensaje1);//3
 }
 mostrarmensaje1();
+let alumno = 'Sergio';
+let nota = '7';
+let espacio = ' ';
+function mostrarnota(){
+    let mensaje4 = 'La nota del alumno: '+nota+ espacio+ 'su nombre'+espacio +alumno +espacio;
+    console.log (mensaje4);//4
+}
+mostrarnota();
  
